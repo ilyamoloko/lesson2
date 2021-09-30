42a8f484a1b148a3cf8208eb0dc81d14c619780e
ilyamoloko <79924234+ilyamoloko@users.noreply.github.com>
2021-09-30 20:02:56 +0300

    Initial commit

---
+++ src/lesson/Main.java
@@ -0,0 +1,52 @@
+package lesson;
+
+import java.awt.*;
+
+public class Main {
+
+    public static void moveRobot(Robot robot, int toX, int toY) {
+
+        int tX = robot.getX();
+        int tY = robot.getY();
+
+
+        if (tX >= toX) {
+            while (robot.getDirection() != Direction.LEFT) {
+                robot.turnLeft();
+            }
+            while (tX != toX) {
+                robot.stepForward();
+                tX--;
+            }
+        } else {
+            while (robot.getDirection() != Direction.RIGHT) {
+                robot.turnRight();
+            }
+            while (tX != toX) {
+                robot.stepForward();
+                tX++;
+            }
+        }
+
+        if (tY >= toY) {
+            while (robot.getDirection() != Direction.DOWN) {
+                robot.turnLeft();
+            }
+            while (tY != toY) {
+                robot.stepForward();
+                tY--;
+            }
+
+        } else {
+            while (robot.getDirection() != Direction.UP) {
+                robot.turnRight();
+            }
+            while (tY != toY) {
+                robot.stepForward();
+                tY++;
+            }
+
+        }
+
+    }
+}
